125045ef7f20a15b8608f671c0e81246e81b4931
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-01 21:04:57 -0500

    only took away one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import BookInfo from "./pages/BookInfo";
-import {  } from "../data";
+import { fetchMovies } from "./components/data";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Cart from "./pages/Cart";
@@ -82,7 +82,7 @@ function App() {
         <Nav numberOfItems={numberOfItems()} />
         <Routes>
           <Route path="/" element={<Home books={books} />} />
-          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books"element={<Books />}/>
           <Route
             path="/books/:id"
             element={<BookInfo books={books} addItemToCart={addItemToCart} />}
